01dd245bc3e78bf58d3a8c4607ae4aeb6ba7f2d6
Aabid Ali <mdaabidali28@gmail.com>
2026-07-05 11:48:48 +0530

    How To Access Array Element

--- TopicWise/Array/Access.java
+++ TopicWise/Array/Access.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 public class Access {   
     public static void main(String [] args){
-        c
+        Scanner  sc = new Scanner(System.in);
+        
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println("Enter the index of element you want to access : ");
+        int index = sc.nextInt();
+        if(index >= 0 && index <arr.length){
+            System.out.println("Element at index " + index + " is : " + arr[index]);
+            
+        } else {
+            System.out.println("Invalid index");
+        }
+
+
     }
+}
